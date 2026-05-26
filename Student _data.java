5ce3bb1223b2b8b39021ac995cea52e58063904c
VeeramalluDinesh <dineshabhiram500@gmail.com>
2026-05-26 20:36:53 +0530

    Create Student _data.java

---
+++ Student _data.java
@@ -0,0 +1,17 @@
+public class Main {
+  public static void main(String[] args) {
+    // Student data
+    String studentName = "Raghavi"
+    int studentID = 6640;
+    int studentAge = 20;
+    float studentFee = 65.00f;
+    char studentGrade = 'A';
+    
+    // Print variables
+    System.out.println("Student name: " + studentName);
+    System.out.println("Student id: " + studentID);
+    System.out.println("Student age: " + studentAge);
+    System.out.println("Student fee: " + studentFee);
+    System.out.println("Student grade: " + studentGrade);
+  }
+}
